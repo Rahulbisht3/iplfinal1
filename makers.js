11f17b1043249680a95656062706a8e385f62785
Rahul <bishtrahul393@gmail.com>
2020-04-08 03:56:45 +0530

    initial commit

---
+++ makers.js
@@ -0,0 +1,50 @@
+const topEconomicalBowler = require("./see/topEconomicalBowler");
+const MATCHES_FILE_PATH = "./csv_data/matches.csv";
+const DELIVERIES_FILE_PATH = "./csv_data/deliveries.csv";
+const csv = require("csvtojson");
+
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+let PORT = process.env.PORT || 3000;
+app.use(bodyParser.json());
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", (request, response) => {
+  response.sendFile("index.html", {
+    root: __dirname + "/public",
+  });
+});
+
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
+
+app.get("/matches-played", (req, res) => {
+  let year = req.query.season;
+  csv()
+    .fromFile(MATCHES_FILE_PATH)
+    .then((matches) => {
+      csv()
+        .fromFile(DELIVERIES_FILE_PATH)
+        .then((deleveries) => {
+          let economicalBowlersResult = topEconomicalBowler(
+            matches,
+            deleveries,
+            year
+          );
+          res.json({
+            economicalBowlersResult,
+          });
+        });
+    });
+});
+
+app.listen(3000, () => {
+  console.log("server started");
+});
---
+++ see/topEconomicalBowler.js
@@ -0,0 +1,44 @@
+function topEconomicalBowler(matches, deliveries, h) {
+  // console.log(h);
+  let setYear = h;
+  let out = {};
+  let see = {};
+  let final = {};
+  for (let match of matches) {
+    if (setYear === match.season) {
+      for (let del of deliveries) {
+        if (match.id === del.match_id) {
+          let run =
+            parseInt(del.total_runs) -
+            (parseInt(del.penalty_runs) +
+              parseInt(del.bye_runs) +
+              parseInt(del.legbye_runs));
+          out[del.bowler] = (out[del.bowler] || 0) + run;
+          let count = parseInt(del.wide_runs) + parseInt(del.noball_runs);
+          if (count === 0) {
+            see[del.bowler] = (see[del.bowler] || 0) + 1;
+          }
+        }
+      }
+    }
+  }
+
+  for (let i in see) {
+    final[i] = (out[i] / see[i]) * 6;
+  }
+  var sortable = [];
+  for (var key in final)
+    if (final.hasOwnProperty(key)) sortable.push([key, final[key]]);
+
+  sortable.sort(function (a, b) {
+    return a[1] - b[1];
+  });
+  let d = {};
+  for (let i = 0; i < 10; i++) {
+    d[sortable[i][0]] = parseFloat(sortable[i][1].toFixed(2));
+  }
+  // console.log(d);
+  return d;
+}
+
+module.exports = topEconomicalBowler;
